45b7e23618723eb659e82d8778c12964641cbdde
belstrel <belstrel@gmail.com>
2019-10-04 08:59:39 +0300

    work read and write files to many docker containers

--- consumer/src/main/scala/com/belstrel/otusanalytics/consumer/CustomConsumer.scala
+++ consumer/src/main/scala/com/belstrel/otusanalytics/consumer/CustomConsumer.scala
@@ -9,12 +9,15 @@ import java.util.Properties
 import com.typesafe.scalalogging.LazyLogging
 
 import scala.collection.JavaConverters._
+import scala.collection.mutable.ListBuffer
+import scala.io.Source
 
 object CustomConsumer  extends App with LazyLogging {
 
 
   override def main(args: Array[String]): Unit = {
     consumeFromKafka("stock-topic")
+
   }
 
   def consumeFromKafka(topic: String) = {
@@ -25,25 +28,71 @@ object CustomConsumer  extends App with LazyLogging {
     props.put("auto.offset.reset", "latest")
     props.put("group.id", "consumer-group")
     Thread.sleep(10100)
-    val consumer: KafkaConsumer[String, String] = new KafkaConsumer[String, String](props)
+//    val consumer: KafkaConsumer[String, String] = new KafkaConsumer[String, String](props)
+//
+//    consumer.subscribe(util.Arrays.asList(topic))
+//    while (true) {
+//      val record = consumer.poll(1000).asScala
+//      for (data <- record.iterator)
+//        println(data.value())
+//    }
+//    consumer.close()
 
+    val consumer: KafkaConsumer[String, String] = new KafkaConsumer[String, String](props)
     consumer.subscribe(util.Arrays.asList(topic))
+
+//    var list = scala.collection.mutable.ArraySeq[String]
+//    sudo  docker exec  -i -t 8e3ed337ad0a /bin/bash
+    //  refactor to csv!!!
+    val file = new File("/stage/model/sec.txt" )
+    file.setExecutable(true)
+    file.setWritable(true)
+    file.setReadable(true)
     while (true) {
       val record = consumer.poll(1000).asScala
-      for (data <- record.iterator)
-        println(data.value())
+      for (data <- record.iterator) {
+        writeFile( file ,  data.value())
+      }
     }
     consumer.close()
 
+    def writeFile(file: File, line: String): Unit = {
+      val bw = new BufferedWriter(new FileWriter(file, true))
+        bw.write(line + "/n")
+      bw.close()
+    }
 
-//    def writeFile(filename: String, s: String): Unit = {
-//      val file = new File(filename)
-//      val bw = new BufferedWriter(new FileWriter(file))
-//      bw.write(s)
-//      bw.close()
-//    }
 
 
 
   }
 }
+
+//val props:Properties = new Properties()
+//props.put("group.id", "test")
+//props.put("bootstrap.servers","localhost:9092")
+//props.put("key.deserializer",
+//"org.apache.kafka.common.serialization.StringDeserializer")
+//Props.put("value.deserializer",
+//"org.apache.kafka.common.serialization.StringDeserializer")
+//props.put("enable.auto.commit", "true")
+//props.put("auto.commit.interval.ms", "1000")
+//val consumer = new KafkaConsumer(props)
+//val topics = List("topic_text")
+//try {
+//  consumer.subscribe(topics.asJava)
+//  while (true) {
+//  val records = consumer.poll(10)
+//  for (record <- records.asScala) {
+//  println("Topic: " + record.topic() +
+//  ",Key: " + record.key() +
+//  ",Value: " + record.value() +
+//  ", Offset: " + record.offset() +
+//  ", Partition: " + record.partition())
+//}
+//}
+//}catch{
+//  case e:Exception => e.printStackTrace()
+//}finally {
+//  consumer.close()
+//}
--- transporter/src/main/scala/com/belstrel/otusanalytics/transporter/DataTransporterApp.scala
+++ transporter/src/main/scala/com/belstrel/otusanalytics/transporter/DataTransporterApp.scala
@@ -84,3 +84,20 @@ object DataTransporterApp extends App with StrictLogging {
 //
 //  })
 }
+
+//try {
+//  for (i <- 0 to 15) {
+//  val record = new ProducerRecord[String, String](topic, i.toString, "My Site is sparkbyexamples.com " + i)
+//  val metadata = producer.send(record)
+//  printf(s"sent record(key=%s value=%s) " +
+//  "meta(partition=%d, offset=%d)\n",
+//  record.key(), record.value(),
+//  metadata.get().partition(),
+//  metadata.get().offset())
+//}
+//}catch{
+//  case e:Exception => e.printStackTrace()
+//}finally {
+//  producer.close()
+//}
+//}
